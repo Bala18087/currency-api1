e860e85dfad138f4dea66b419b0515f64167e281
Bala18087 <116589845+Bala18087@users.noreply.github.com>
2025-11-02 14:12:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import quotesRouter from "./routes/quotes.js";
+import quotesRouter from "./quotes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
